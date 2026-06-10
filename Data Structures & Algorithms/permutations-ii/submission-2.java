1543c8e70fb46fb06cce0afd4ade8a03ebda6686
Abdellah Kamil <abdellah.kamil2017@gmail.com>
2026-06-10 21:04:56 +0300

    Add: permutations-ii - submission-2

---
+++ Data Structures & Algorithms/permutations-ii/submission-2.java
@@ -0,0 +1,26 @@
+class Solution {
+    List<List<Integer>> res;
+    public List<List<Integer>> permuteUnique(int[] nums) {
+        res = new ArrayList<>();
+        Arrays.sort(nums);
+        backtrack(new ArrayList<>(), nums, new boolean[nums.length]);
+        return res;
+    }
+
+    private void backtrack(List<Integer> perm, int[] nums, boolean[] pick) {
+        if (perm.size() == nums.length) {
+            res.add(new ArrayList<>(perm));
+            return;
+        }
+
+        for (int i = 0; i < nums.length; i++) {
+            if (pick[i] || (i > 0 && nums[i] == nums[i - 1] && !pick[i - 1])) continue;
+            perm.add(nums[i]);
+            pick[i] = true;
+            backtrack(perm, nums, pick);
+
+            perm.remove(perm.size() - 1);
+            pick[i] = false;
+        }
+    }
+}
